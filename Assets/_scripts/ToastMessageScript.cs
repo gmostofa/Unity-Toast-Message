54ec2280562d9417385f880e2e9549a40105ae8a
gmostofa <mostofart@gmail.com>
2017-02-16 12:04:04 +0600

    inital commit

---
+++ Assets/_scripts/ToastMessageScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class ToastMessageScript : MonoBehaviour
+{
+    string toastString;
+    AndroidJavaObject currentActivity;
+    AndroidJavaClass UnityPlayer;
+    AndroidJavaObject context;
+
+    static ToastMessageScript instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public static ToastMessageScript Controller()
+    {
+            return instance;
+    }
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+        }
+    }
+   
+    public void showToastOnUiThread(string toastString)
+    {
+        this.toastString = toastString;
+        currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(ShowToast));
+    }
+
+    void ShowToast()
+    {
+        Debug.Log(this + ": Running on UI thread");
+
+        AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
+        AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String", toastString);
+        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_SHORT"));
+        toast.Call("show");
+    }
+}
---
+++ Assets/_scripts/User.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class User : MonoBehaviour {
+    public Button show;
+
+	void Start () {
+        show.onClick.AddListener(delegate{
+           ToastMessageScript.Controller().showToastOnUiThread("Successfully showed.");
+        });
+	}
+}
